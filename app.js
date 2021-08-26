d724eda979f8bcf892b09c314e668bc1831ba5fe
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-08-26 08:55:51 -0700

    "created random functionality for each function "

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 function randomDestination(array){
-
+  let item = array[Math.floor(Math.random()*array.length)];
+  console.log(item);
 }
 
-randomDestination(["park", "eagle's stadium", "pike place"]);
+randomDestination(["park", "eagle's stadium", "pike's place"]);
 
 function randomRestaurant(array){
-
+  let item = array[Math.floor(Math.random()*array.length)];
+  console.log(item);
 }
 
 randomRestaurant(["wendys", "mc donalds", "taco bell"]);
 
 function randomTransportation(array) {
-
+  let item = array[Math.floor(Math.random()*array.length)];
+  console.log(item);
 }
 
 randomTransportation(["taxi", "rent a car", "uber"]);
 
 function randomEntertainment(array) {
-
+  let item = array[Math.floor(Math.random()*array.length)];
+  console.log(item);
 }
 
 randomEntertainment(["comic con", "sakura con", "hatsune miku expo"]);
