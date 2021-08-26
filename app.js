07f00eca8ba430f6876b063c39321a9df659ed14
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-08-26 09:53:30 -0700

    "made buttons and user view"

--- app.js
+++ app.js
@@ -1,28 +1,39 @@
-function randomDestination(array){
+function randomDestination(){
+  let array = ["park", "eagle's stadium", "pike's place"];
   let item = array[Math.floor(Math.random()*array.length)];
   console.log(item);
+  document.getElementById("destination").innerHTML = `your current destination is ${item}`;
+
 }
 
-randomDestination(["park", "eagle's stadium", "pike's place"]);
+randomDestination();
 
-function randomRestaurant(array){
+function randomRestaurant(){
+  let array = ["wendys", "mc donalds", "taco bell"];
   let item = array[Math.floor(Math.random()*array.length)];
   console.log(item);
+  document.getElementById("restaurant").innerHTML = `your current eatery is ${item}`;
 }
 
-randomRestaurant(["wendys", "mc donalds", "taco bell"]);
+randomRestaurant();
 
-function randomTransportation(array) {
+function randomTransportation() {
+  let array = ["taxi", "rent a car", "uber"];
   let item = array[Math.floor(Math.random()*array.length)];
   console.log(item);
+  document.getElementById("transportation").innerHTML = `your current mode of transport is ${item}`;
+
 }
 
-randomTransportation(["taxi", "rent a car", "uber"]);
+randomTransportation();
 
-function randomEntertainment(array) {
+function randomEntertainment() {
+  let array = ["comic con", "sakura con", "hatsune miku expo"];
   let item = array[Math.floor(Math.random()*array.length)];
   console.log(item);
+  document.getElementById("convention").innerHTML = `your current eatery is ${item}`;
+
 }
 
-randomEntertainment(["comic con", "sakura con", "hatsune miku expo"]);
+randomEntertainment();
 
