4cfe707a56cbb75309a85de886e43363919c4a26
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-08-26 08:45:16 -0700

    "created random generator functions outline"

---
+++ app.js
@@ -0,0 +1,24 @@
+function randomDestination(array){
+
+}
+
+randomDestination(["park", "eagle's stadium", "pike place"]);
+
+function randomRestaurant(array){
+
+}
+
+randomRestaurant(["wendys", "mc donalds", "taco bell"]);
+
+function randomTransportation(array) {
+
+}
+
+randomTransportation(["taxi", "rent a car", "uber"]);
+
+function randomEntertainment(array) {
+
+}
+
+randomEntertainment(["comic con", "sakura con", "hatsune miku expo"]);
+
