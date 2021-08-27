322024aae26e838bd5a479a0d4cde124d1a5a7ec
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-08-26 20:41:22 -0700

    "many improvements"

--- app.js
+++ app.js
@@ -1,39 +1,89 @@
-function randomDestination(){
-  let array = ["park", "eagle's stadium", "pike's place"];
-  let item = array[Math.floor(Math.random()*array.length)];
-  console.log(item);
-  document.getElementById("destination").innerHTML = `your current destination is ${item}`;
 
+const destinations = ["Pike's Place", "1st street", "Space needle"];
+
+const restaurants = {
+  "Pike's Place": ["Wendy's", "Denny's", "Burger King"],
+  "1st street": ["Taco Bell", "Fancy Fish", "Local Pho"],
+  "Space needle": ["Spicy Shrimp", "Pumpkin Pies 'R Us"]
 }
 
-randomDestination();
-
-function randomRestaurant(){
-  let array = ["wendys", "mc donalds", "taco bell"];
-  let item = array[Math.floor(Math.random()*array.length)];
-  console.log(item);
-  document.getElementById("restaurant").innerHTML = `your current eatery is ${item}`;
+function isYes(input) {
+  return input === "yes";
 }
 
-randomRestaurant();
-
-function randomTransportation() {
-  let array = ["taxi", "rent a car", "uber"];
-  let item = array[Math.floor(Math.random()*array.length)];
+function getRandom(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  const item = array[randomIndex];
   console.log(item);
-  document.getElementById("transportation").innerHTML = `your current mode of transport is ${item}`;
-
+  return item;
 }
 
-randomTransportation();
-
-function randomEntertainment() {
-  let array = ["comic con", "sakura con", "hatsune miku expo"];
-  let item = array[Math.floor(Math.random()*array.length)];
-  console.log(item);
-  document.getElementById("convention").innerHTML = `your current eatery is ${item}`;
-
+function selectDestination() {
+  let destination = getRandom(destinations);
+  while (!isYes(prompt("are you happy with your destination type yes or no"))) {
+    destination = getRandom(destinations);
+  }
+  return destination;
 }
 
-randomEntertainment();
+function selectRestaurant(destination) {
+  const restaurantsForDestination = restaurants[destination];
+  let restaurant = getRandom(restaurantsForDestination);
+  while (!isYes(prompt("are you happy with your food choice type yes or no"))) {
+    restaurant = getRandom(restaurantsForDestination);
+  }
+  return restaurant;
+}
 
+function completeTrip() {
+  const destination = selectDestination();
+  const restaurant = selectRestaurant(destination);
+  console.log("Trip is confirmed. Please have a nice vacation. No Refunds.");
+  console.log(destination);
+  console.log(restaurant);
+}
+
+completeTrip();
+
+
+// function randomTransportation() {
+//   let array = ["taxi", "rent a car", "uber"];
+//   let transportMode = array[Math.floor(Math.random()*array.length)];
+//   console.log(transportMode);
+//
+// }
+//
+//
+// function randomEntertainment() {
+//   let array = ["comic con", "sakura con", "hatsune miku expo"];
+//   let con = array[Math.floor(Math.random()*array.length)];
+//   console.log(con);
+//   return con;
+//
+// }
+
+
+// function randomRestaurant(incomingItems){
+//
+//   let index = [Math.floor(Math.random()*incomingItems.length)];
+//   // console.log(eatery);
+//   return eatery = incomingItems[index];
+// }
+// let restaurantArray = ["Wendy's", "Dennys", "BurgerKing"];
+// let restResult = randomRestaurant(restaurantArray);
+// console.log(restResult);
+
+
+// function selectRestaurant(destination) {
+//   let restaurantsArray = ["Wendy's", "Dennys", "BurgerKing"];
+//   let restResult = randomArray(restaurantsArray);
+//   let resHappy = false;
+//   while (!resHappy) {
+//     if (isYes(prompt("are you happy with your food choice type yes or no"))) {
+//       resHappy = true;
+//     } else {
+//       restResult = randomArray(restaurantsArray);
+//     }
+//   }
+//   return restResult;
+// }
